17d10951e43a83e311b2fda73d123d40b90dce33
DiegoFR <diegofreire8482@hotmail.com>
2020-03-30 17:51:31 -0300

    teste

---
+++ Principal2.java
@@ -0,0 +1,11 @@
+package principal;
+import negocio.Logica;
+
+public class Principal {
+	public static void main(String[] args) {
+		Logica jogo = new Logica();
+
+		//Aqui chama-se o método para dar inicio ao jogo.
+		jogo.inicia();
+	}
+}
